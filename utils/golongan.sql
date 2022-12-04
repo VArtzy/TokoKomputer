294709c48f9be10fca903788d7907614bf42dff0
VArtzy <farrelnikoson@gmail.com>
2022-12-04 12:06:55 +0700

    add kecuali sql

--- utils/golongan.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `golongan` (
-  `KODE` varchar(6) NOT NULL,
-  `KETERANGAN` varchar(50) DEFAULT NULL
-) ;
-
-ALTER TABLE `golongan`
-  ADD PRIMARY KEY (`KODE`);
-
--- utils/wilayah.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `wilayah` (
-  `KODE` varchar(5) NOT NULL,
-  `KETERANGAN` varchar(50) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-ALTER TABLE `wilayah`
-  ADD PRIMARY KEY (`KODE`);
