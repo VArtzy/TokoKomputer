6f3437c19ed01eb54d77aa0fbb3497d1057455e5
VArtzy <farrelnikoson@gmail.com>
2022-12-04 15:38:28 +0700

    fix beli #3

--- pilihBarangBeli.php
+++ pilihBarangBeli.php
@@ -14,8 +14,9 @@ include('shared/navadmin.php');
     <h1 class="text-2xl font-semibold mb-4">Pilih Barang</h1>
     <a class="btn btn-primary mb-8" href="tambahNotaBeli.php">Lanjut Checkout</a>
     <button class="btn btn-error mb-8 btn-clear-cart">Reset Keranjang</button>
-    <a class="btn btn-warning mb-8" href="invoices.php">Kembali</a>
-
+    <a class="btn btn-warning mb-8" href="beli.php">Kembali</a>
+    <span>Atau</span>
+    <a class="btn btn-warning mb-8" href="tambahBarang.php">Buat Barang</a>
     <div class="">
         <input type="text" name="keyword" size="40" class="input input-bordered max-w-xs mr-2" autofocus placeholder="Masukkan Keyword Nama/Kode/Harga" autocomplete="off" id="keyword">
         <button type="submit" name="cari" class="opacity-50" id="tombol-cari">Cari</button>
